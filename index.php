608679c476a3515ce4e383818e0114894ac05160
EmmanuelMmanda <luneya17@gmail.com>
2022-12-22 13:48:55 +0300

    ussd

--- index.php
+++ index.php
@@ -5,31 +5,33 @@ $serviceCode = $_POST["serviceCode"];
 $phoneNumber = $_POST["phoneNumber"];
 $text = $_POST["text"];
 
-if ($text == "") {
-    // This is the first request. Note how we start the response with CON
-    $response = "CON What would you want to check \n";
-    $response .= "1. My Account \n";
-    $response .= "2. My phone number";
+$isNotRegistered = true;
+$username = "Emmanuel Mmanda";
+$balance = "100,000 Tsh";
 
-} else if ($text == "1") {
-    // Business logic for first level response
-    $response = "CON Choose account information you want to view \n";
-    $response .= "1. Account number \n";
+if ($text == "" && $isNotRegistered) {
+    # initial and user not registered
+    $response = "CON Welcome to VICOBA, Choose 1 to Register: \n";
+    $response .= "1. Register";
 
-} else if ($text == "2") {
-    // Business logic for first level response
-    // This is a terminal request. Note how we start the response with END
-    $response = "END Your phone number is " . $phoneNumber;
+} elseif ($text == "" && !$isNotRegistered) {
+    # initial and user is registered
+    $response = "CON Welcome to VICOBA,.$username.  \n";
+    $response .= "1. Send Money \n";
+    $response .= "2. Withdraw Money \n";
+    $response .= "3. Check Balance \n";
+} elseif ($text == "1") {
+    # sending money
+    $response = "END We will allow send Money Shorlty \n";
+} elseif ($text == "2") {
+    #withdrawing money
+    $response = "END We will allow withdraw Money Shorlty \n";
 
-} else if ($text == "1*1") {
-    // This is a second level response where the user selected 1 in the first instance
-    $accountNumber = "ACC1001";
+} elseif ($text == "3") {
+    #checking balance
 
-    // This is a terminal request. Note how we start the response with END
-    $response = "END Your account number is " . $accountNumber;
+    $response = "END Your Current balance is: .$balance.\n";
 
 }
 
-// Echo the response back to the API
-header('Content-type: text/plain');
-echo $response;
+?>
